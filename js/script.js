0e9135f3775a8925e110d097866ffd232eb64a0d
Jack <jackchen8888@gmail.com>
2023-01-24 00:26:01 +0100

    Code Formatting

--- js/script.js
+++ js/script.js
@@ -38,7 +38,6 @@ var dicePool = {
 		board_size: 87 // 121
 	}
 };
-
 var expansions = {
 	"none": {
 		dice: [],
@@ -51,8 +50,7 @@ var expansions = {
 		effective_dice: 0
 	},
 	"alien-farmer": {
-		dice: ["Future/images/future_alienFarmerA_"
-		],
+		dice: ["Future/images/future_alienFarmerA_"],
 		effective_dice: 1
 	},
 	"canyon": {
@@ -68,8 +66,7 @@ var expansions = {
 		effective_dice: 1
 	},
 	"construction": {
-		dice: ["Engineer/images/engineer_constructionA_"
-		],
+		dice: ["Engineer/images/engineer_constructionA_"],
 		effective_dice: 0
 	},
 	"desert": {
@@ -85,8 +82,7 @@ var expansions = {
 		effective_dice: 2
 	},
 	"galactic-invaders": {
-		dice: ["Arcade/images/arcade_galacticInvaderA_"
-		],
+		dice: ["Arcade/images/arcade_galacticInvaderA_"],
 		effective_dice: 0
 	},
 	"investigation": {
@@ -112,13 +108,11 @@ var expansions = {
 		effective_dice: 1
 	},
 	"pluck-man": {
-		dice: ["Arcade/images/arcade_plucManA_"
-		],
+		dice: ["Arcade/images/arcade_plucManA_"],
 		effective_dice: 0
 	},
 	"portal": {
-		dice: ["Cthulu/images/cthulu_portalA_"
-		],
+		dice: ["Cthulu/images/cthulu_portalA_"],
 		effective_dice: 1
 	},
 	"power-grid": {
@@ -148,13 +142,11 @@ var expansions = {
 		effective_dice: 2
 	},
 	"separation": {
-		dice: ["Engineer/images/engineer_separationA_"
-		],
+		dice: ["Engineer/images/engineer_separationA_"],
 		effective_dice: 0
 	},
 	"special": {
-		dice: ["Engineer/images/engineer_specialA_"
-		],
+		dice: ["Engineer/images/engineer_specialA_"],
 		effective_dice: 0
 	},
 	"street-lamps": {
@@ -164,18 +156,15 @@ var expansions = {
 		effective_dice: 2
 	},
 	"superconnection": {
-		dice: ["Future/images/future_superconnectionA_"
-		],
+		dice: ["Future/images/future_superconnectionA_"],
 		effective_dice: 1
 	},
 	"tentacle": {
-		dice: ["Cthulu/images/cthulu_tentacleA_"
-		],
+		dice: ["Cthulu/images/cthulu_tentacleA_"],
 		effective_dice: 0
 	},
 	"tetromino": {
-		dice: ["Arcade/images/arcade_tetrominoA_"
-		],
+		dice: ["Arcade/images/arcade_tetrominoA_"],
 		effective_dice: 0
 	},
 	"trail": {
@@ -199,11 +188,9 @@ var expansions = {
 		effective_dice: 0
 	}
 };
-
 var currentRound = 1;
 
 function rollDice() {
-	
 	var mainSetting = document.getElementById("main-setting").value;
 	var firstExpansion = document.getElementById("first-expansion").value;
 	var secondExpansion = document.getElementById("second-expansion").value;
@@ -211,72 +198,65 @@ function rollDice() {
 	var effective_die = dicePool[mainSetting].effective_dice + expansions[firstExpansion].effective_dice + expansions[secondExpansion].effective_dice;
 	var maxRounds = Math.floor(dicePool[mainSetting].board_size / effective_die);
 	var border = (dicePool[mainSetting].dice).length
-	
 	roundImg = createSquare(200, '#202124', "Round " + currentRound + "/" + maxRounds);
-	
 	if (currentRound <= maxRounds) {
 		currentRound += 1;
 		var container = document.createElement("div");
 		container.appendChild(roundImg)
 		var row = document.createElement("div");
-
 		for (var i = 0; i < currentDicePool.length; i++) {
-		
 			if (i == border) {
 				row.setAttribute('onclick', "spoiler(this)")
 				row.classList.add("disable-dbl-tap-zoom", "blur", "image-row");
 				container.appendChild(row);
-				var row = document.createElement("div");	
+				var row = document.createElement("div");
 			}
-			var roll = Math.floor(rand(1,6));
+			var roll = Math.floor(rand(1, 6));
 			var diceImg = document.createElement("img");
 			diceImg.height = 200;
 			diceImg.width = 200;
 			diceImg.src = "dice/" + currentDicePool[i] + roll + ".png";
 			row.appendChild(diceImg);
+		}
+		row.setAttribute('onclick', "spoiler(this)")
+		row.classList.add("disable-dbl-tap-zoom", "blur", "image-row");
+		container.classList.add("row-container");
+		container.appendChild(row)
+		resCon.appendChild(container);
 	}
-	row.setAttribute('onclick', "spoiler(this)")
-	row.classList.add("disable-dbl-tap-zoom", "blur", "image-row");
-	container.classList.add("row-container");
-	container.appendChild(row)
-	resCon.appendChild(container);
-	}
-	
 }
 
-function rand(min, max){
-    return (Math.floor(Math.pow(10,14)*Math.random()*Math.random())%(max-min+1))+min;
+function rand(min, max) {
+	return (Math.floor(Math.pow(10, 14) * Math.random() * Math.random()) % (max - min + 1)) + min;
 }
 
 function createSquare(size, color, text) {
-  var square = document.createElement("div");
-  var textNode = document.createTextNode(text);
-  square.style.color = "white";
-  square.style.backgroundColor = color;
-  square.appendChild(textNode);
-  square.style.fontSize = 25 + 'px';
-  square.style.height = 25 + 'px';
-  square.style.lineHeight = 25 + 'px';
-  return square;
+	var square = document.createElement("div");
+	var textNode = document.createTextNode(text);
+	square.style.color = "white";
+	square.style.backgroundColor = color;
+	square.appendChild(textNode);
+	square.style.fontSize = 25 + 'px';
+	square.style.height = 25 + 'px';
+	square.style.lineHeight = 25 + 'px';
+	return square;
 }
-
 
 function unhide_elem(elem) {
 	var element = document.getElementById(elem);
-  	element.classList.remove('hidden');
+	element.classList.remove('hidden');
 }
 
 function hide_elem(elem) {
 	var element = document.getElementById(elem);
-  	element.classList.add('hidden');
+	element.classList.add('hidden');
 }
 
 function spoiler(elem) {
-  	elem.classList.toggle('blur');
+	elem.classList.toggle('blur');
 }
 
-function clearBox(elementID)
-{
+function clearBox(elementID) {
 	currentRound = 1;
-    document.getElementById(elementID).innerHTML = "";
+	document.getElementById(elementID).innerHTML = "";
 }
