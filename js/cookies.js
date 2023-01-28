2e3baef63d63b47279c33037e67d93adff1c843b
Jack <jackchen8888@gmail.com>
2023-01-28 04:53:59 +0100

    beautify

--- js/cookies.js
+++ js/cookies.js
@@ -7,19 +7,16 @@ window.onload = function() {
 		document.getElementById("second-expansion").value = getLocalStorage('secondExpansion');
 	}
 };
-
 // function to save data to local storage
 function saveProgress() {
 	var mainSetting = document.getElementById("main-setting");
 	var firstExpansion = document.getElementById("first-expansion");
 	var secondExpansion = document.getElementById("second-expansion");
-	
 	localStorage.setItem('pageHTML', document.documentElement.innerHTML);
 	localStorage.setItem('mainSetting', mainSetting.value);
 	localStorage.setItem('firstExpansion', firstExpansion.value);
 	localStorage.setItem('secondExpansion', secondExpansion.value);
 }
-
 // function to load data from local storage
 function getLocalStorage(key) {
 	return localStorage.getItem(key);
