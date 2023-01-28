c3e92f899cd80214f2bbbe59aacca31aa1867bfa
Jack <jackchen8888@gmail.com>
2023-01-28 03:15:17 +0100

    cookies attempt 2

--- js/cookies.js
+++ js/cookies.js
@@ -1,29 +1,15 @@
 window.addEventListener('load', function() {
-    var htmlContent = getCookie('html_content');
-    if (htmlContent) {
-        document.documentElement.innerHTML = htmlContent;
-    }
+    var htmlContent = getCookie('html_content');document.documentElement.outerHTML = getLocalStorage('pageHTML');
 });
 
-function setCookie(name, value, days) {
-    var expires = "";
-    if (days) {
-        var date = new Date();
-        date.setTime(date.getTime() + (days*24*60*60*1000));
-        expires = "; expires=" + date.toUTCString();
-    }
-    document.cookie = name + "=" + (value || "")  + expires + "; path=/";
+// function to save data to local storage
+function setLocalStorage(key, value) {
+  localStorage.setItem(key, value);
 }
 
-function getCookie(name) {
-    var nameEQ = name + "=";
-    var ca = document.cookie.split(';');
-    for(var i=0;i < ca.length;i++) {
-        var c = ca[i];
-        while (c.charAt(0)==' ') c = c.substring(1,c.length);
-        if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
-    }
-    return null;
+// function to load data from local storage
+function getLocalStorage(key) {
+  return localStorage.getItem(key);
 }
 
 
