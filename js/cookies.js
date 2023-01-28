4749bebf8a407a4eee9f8d4f41b40cdbfdc92ff9
Jack <jackchen8888@gmail.com>
2023-01-28 03:25:21 +0100

    cookies attempt 4

--- js/cookies.js
+++ js/cookies.js
@@ -1,8 +1,8 @@
 window.addEventListener('load', function() {
-	if (localStorage.getItem('pageHTML') !== null) {
-		// load the saved HTML from local storage and set it as the HTML of the page
-		document.documentElement.outerHTML = getLocalStorage('pageHTML');
-	}
+	if(localStorage.getItem('pageHTML') !== null) {
+  // load the saved HTML from local storage and set it as the HTML of the body element
+  document.body.innerHTML = getLocalStorage('pageHTML');
+}
 });
 // function to save data to local storage
 function setLocalStorage(key, value) {
