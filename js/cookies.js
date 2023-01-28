b6e97418a6ec94751129bca49b187814ef6c8bd5
Jack <jackchen8888@gmail.com>
2023-01-28 03:46:34 +0100

    cookies attempt 5

--- js/cookies.js
+++ js/cookies.js
@@ -1,14 +1,17 @@
 window.addEventListener('load', function() {
-	if(localStorage.getItem('pageHTML') !== null) {
-  // load the saved HTML from local storage and set it as the HTML of the body element
-  document.body.innerHTML = getLocalStorage('pageHTML');
-}
+	if (localStorage.getItem('pageHTML') !== null) {
+		// load the saved HTML from local storage and set it as the HTML of the body element
+		document.body.innerHTML = getLocalStorage('pageHTML');
+	}
 });
 // function to save data to local storage
-function setLocalStorage(key, value) {
-	localStorage.setItem(key, value);
+function saveProgress(key, value) {
+	localStorage.setItem('pageHTML', document.documentElement.innerHTML);
+	localStorage.setItem('mainSetting', mainSetting);
+	localStorage.setItem('firstExpansion', firstExpansion);
+	localStorage.setItem('secondExpansion', secondExpansion);
 }
 // function to load data from local storage
-function getLocalStorage(key) {
+function loadProgress(key) {
 	return localStorage.getItem(key);
 }
